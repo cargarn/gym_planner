664b69d59bd342188bf5796f0dafe400996f42af
CarlosGarnacho <carlos_garnacho@hotmail.com>
2022-08-05 08:59:20 +0200

    Included error handlers in backend api calls

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,44 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc	Get goals
 // @route	GET /api/goals
 // @access	Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({'message': 'Get Goals'});
-}
+})
+
+// @desc	Get goal by id
+// @route	GET /api/goals/:id
+// @access	Private
+const getGoal = asyncHandler(async (req, res) => {
+	res.status(200).json({'message': `Get goal with id: ${req.params.id}`});
+})
 
 // @desc	Set goal
 // @route	POST /api/goals
 // @access	Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+	if (!req.body.text) {
+		res.status(400); //.json({'message': 'Please add a text field'});
+		throw new Error('Please add a text field');
+	}
 	res.status(200).json({'message': 'Set Goal'});
-}
+})
 
-// @desc	Update goal
+// @desc	Update goal by id
 // @route	PUT /api/goals/:id
 // @access	Private
-const updateGoal = (req, res) => {
-	res.status(200).json({'message': `Update goal ${req.params.id}`});
-}
+const updateGoal = asyncHandler(async (req, res) => {
+	res.status(200).json({'message': `Update goal with id: ${req.params.id}`});
+})
 
-// @desc	Delete goal
+// @desc	Delete goal by id
 // @route	DELETE /api/goals/:id
 // @access	Private
-const deleteGoal = (req, res) => {
-	res.status(200).json({'message': `Delete goal ${req.params.id}`});
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+	res.status(200).json({'message': `Delete goal with id: ${req.params.id}`});
+})
 
 module.exports = {
-	getGoals, setGoal, updateGoal, deleteGoal
+	getGoals, getGoal, setGoal, updateGoal, deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const { getGoals, getGoal, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').get(getGoals).put( updateGoal).delete(deleteGoal)
+router.get('/', getGoals)
+router.post('/', setGoal)
+router.get('/:id', getGoal)
+router.put('/:id', updateGoal)
+router.delete('/:id', deleteGoal)
 
 module.exports = router
