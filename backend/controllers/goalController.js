723d488c2373bc08fffa031a4263f39984ed0626
CarlosGarnacho <carlos_garnacho@hotmail.com>
2022-08-05 15:08:24 +0200

    Initial CRUD REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,17 +1,27 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc	Get goals
 // @route	GET /api/goals
 // @access	Private
 const getGoals = asyncHandler(async (req, res) => {
-	res.status(200).json({'message': 'Get Goals'});
+	const goals = await Goal.find({});
+
+	res.status(200).json(goals);
 })
 
 // @desc	Get goal by id
 // @route	GET /api/goals/:id
 // @access	Private
 const getGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({'message': `Get goal with id: ${req.params.id}`});
+	const goal = await Goal.findById(req.params.id);
+
+	if (!goal) {
+		res.status(400);
+		throw new Error('Goal not found');
+	}
+	res.status(200).json(goal);
 })
 
 // @desc	Set goal
@@ -22,21 +32,39 @@ const setGoal = asyncHandler(async (req, res) => {
 		res.status(400); //.json({'message': 'Please add a text field'});
 		throw new Error('Please add a text field');
 	}
-	res.status(200).json({'message': 'Set Goal'});
+
+	const goal = await Goal.create({
+		text: req.body.text,
+	});
+	res.status(200).json(goal);
 })
 
 // @desc	Update goal by id
 // @route	PUT /api/goals/:id
 // @access	Private
 const updateGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({'message': `Update goal with id: ${req.params.id}`});
+	const goal = await Goal.findById(req.params.id);
+
+	if (!goal) {
+		res.status(400);
+		throw new Error('Goal not found');
+	}
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+	res.status(200).json(updatedGoal);
 })
 
 // @desc	Delete goal by id
 // @route	DELETE /api/goals/:id
 // @access	Private
 const deleteGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({'message': `Delete goal with id: ${req.params.id}`});
+	const goal = await Goal.findById(req.params.id);
+
+	if (!goal) {
+		res.status(400);
+		throw new Error('Goal to be deleted not found');
+	}
+	await goal.remove();
+	res.status(200).json({id: req.params.id});
 })
 
 module.exports = {
